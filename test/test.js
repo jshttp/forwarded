0fa39b95177160a451f8edd66d20c416e824e44a
Douglas Christopher Wilson <doug@somethingdoug.com>
2018-10-09 23:10:42 -0400

    tests: replace deprecated assert.deepEqual with deep-equal

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var assert = require('assert')
+var deepEqual = require('deep-equal')
 var forwarded = require('..')
 
 describe('forwarded(req)', function () {
@@ -10,28 +11,28 @@ describe('forwarded(req)', function () {
 
   it('should work with X-Forwarded-For header', function () {
     var req = createReq('127.0.0.1')
-    assert.deepEqual(forwarded(req), ['127.0.0.1'])
+    assert.ok(deepEqual(forwarded(req), ['127.0.0.1']))
   })
 
   it('should include entries from X-Forwarded-For', function () {
     var req = createReq('127.0.0.1', {
       'x-forwarded-for': '10.0.0.2, 10.0.0.1'
     })
-    assert.deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2'])
+    assert.ok(deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2']))
   })
 
   it('should skip blank entries', function () {
     var req = createReq('127.0.0.1', {
       'x-forwarded-for': '10.0.0.2,, 10.0.0.1'
     })
-    assert.deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2'])
+    assert.ok(deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2']))
   })
 
   it('should trim leading OWS', function () {
     var req = createReq('127.0.0.1', {
       'x-forwarded-for': ' 10.0.0.2 ,  , 10.0.0.1 '
     })
-    assert.deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2'])
+    assert.ok(deepEqual(forwarded(req), ['127.0.0.1', '10.0.0.1', '10.0.0.2']))
   })
 })
 
